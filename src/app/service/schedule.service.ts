6cb2c1c5e845a4a07e147f06e1ea655b54c58402
jonasbender <contact@jonasbender.net>
2023-01-21 23:23:35 +0100

    getCurrentSchedule Method in scheduleservice

--- src/app/service/schedule.service.ts
+++ src/app/service/schedule.service.ts
@@ -1,9 +1,22 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ScheduleService {
+  globalId = "de:09162:1180";
+  offsetInMinutes = 0;
+  limit = 10;
+  transportTypes = "UBAHN,BUS"
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getCurrentSchedule() {
+
+    return this.http.get('https://www.mvg.de/api/fib/v2/departure?globalId='+ this.globalId +'&limit=' + this.limit + '&offsetInMinutes=' + this.offsetInMinutes + '&transportTypes=' + this.transportTypes)
+  }
+
 }
