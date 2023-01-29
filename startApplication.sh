625081fa55f9f20383f766b755a541875cbc58a5
jonasbender <contact@jonasbender.net>
2023-01-29 23:25:21 +0100

    added custom port and start script

---
+++ startApplication.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+# bash script to start an angular application on a custom port and name
+echo -e "This is a test"
+
+cd ~/Coding/mvv-departure-schedule-fixed
+
+ng serve --host localhost --port 5001 
+
+cd ..
