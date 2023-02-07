2f37c30705ecce06587ed1aea3e64c3d85ce6231
jonasbender <contact@jonasbender.net>
2023-02-07 23:02:04 +0100

    added checkbox button

--- src/app/service/schedule.service.ts
+++ src/app/service/schedule.service.ts
@@ -5,6 +5,7 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class ScheduleService {
+  return: any;
   globalId = "de:09162:1180";
   offsetInMinutes = 2;
   limit = 10;
@@ -19,8 +20,9 @@ export class ScheduleService {
     return this.http.get('https://www.mvg.de/api/fahrinfo/location/query?q=' + stationName);
   }
 
-  getCurrentSchedule() {
+  
 
+  getCurrentSchedule() {
     return this.http.get('https://www.mvg.de/api/fib/v2/departure?globalId='+ this.globalId +'&limit=' + this.limit + '&offsetInMinutes=' + this.offsetInMinutes + '&transportTypes=' + this.transportTypes)
   }
 
